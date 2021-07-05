6778b133de1a539483f0b2e0bccf044d845817f7
Gus <gusbdev@gmail.com>
2021-07-05 15:26:26 -0300

    Panel component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopComponent } from '../app/top/top.component';
+import { PanelComponent } from './panel/panel.component';
 
 @NgModule({
-  declarations: [AppComponent, TopComponent],
+  declarations: [AppComponent, TopComponent, PanelComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
