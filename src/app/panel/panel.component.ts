efcd4a0cf4bde4417d7c1477539cf812edb4cc1b
Gus <gusbdev@gmail.com>
2021-07-06 17:32:29 -0300

    String interpolation in panel component

--- src/app/panel/panel.component.ts
+++ src/app/panel/panel.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Sentence } from '../shared/sentence.model';
+import { Sentences } from './sentences-mock';
+
 @Component({
   selector: 'app-panel',
   templateUrl: './panel.component.html',
-  styleUrls: ['./panel.component.css']
+  styleUrls: ['./panel.component.css'],
 })
 export class PanelComponent implements OnInit {
+  public phrases: Sentence[] = Sentences;
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/panel/sentences-mock.ts
+++ src/app/panel/sentences-mock.ts
@@ -1,6 +1,6 @@
 import { Sentence } from '../shared/sentence.model';
 
-const Sentences: Sentence[] = [
+export const Sentences: Sentence[] = [
   { sentenceEng: 'I like to learn', sentencePtBr: 'Eu gosto de aprender' },
   { sentenceEng: 'I watch tv', sentencePtBr: 'Eu assisto tv' },
   { sentenceEng: 'How are you?', sentencePtBr: 'Como vai você' },
