2dcd071783ae9bc7288269400c6f9a88f2bc4d8e
Gus <gusbdev@gmail.com>
2021-07-06 10:29:52 -0300

    Property bind

--- src/app/attempts/attempts.component.ts
+++ src/app/attempts/attempts.component.ts
@@ -3,13 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-attempts',
   templateUrl: './attempts.component.html',
-  styleUrls: ['./attempts.component.css']
+  styleUrls: ['./attempts.component.css'],
 })
 export class AttemptsComponent implements OnInit {
+  public emptyHeart: string = '/assets/coracao_vazio.png';
+  public fullHeart: string = '/assets/coracao_cheio.png';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
